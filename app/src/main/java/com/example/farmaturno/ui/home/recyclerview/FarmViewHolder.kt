e4651572b393f8a2004fa13d7399f262ce208358
Juan Acuna <juan0acu@gmail.com>
2023-09-21 22:57:34 -0300

    Add boton llamar, con número e intent para llamada

--- app/src/main/java/com/example/farmaturno/ui/home/recyclerview/FarmViewHolder.kt
+++ app/src/main/java/com/example/farmaturno/ui/home/recyclerview/FarmViewHolder.kt
@@ -1,5 +1,7 @@
 package com.example.farmaturno.ui.home.recyclerview
 
+import android.content.Intent
+import android.net.Uri
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.farmaturno.data.model.FarmaDataResponse
@@ -14,5 +16,12 @@ class FarmViewHolder (view: View):RecyclerView.ViewHolder(view) {
         binding.txtLocalDirection.text = listadoFarmacias.farmacia_direccion
         binding.timeOpenFarm.text = listadoFarmacias.farmacia_apertura
         binding.timeCloseFarm.text = listadoFarmacias.farmacia_cierre
+        binding.btLlamar.setOnClickListener {
+            listadoFarmacias.farmacia_telefono?.let { number ->
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:$number")
+                it.context.startActivity(intent)
+            }
+        }
     }
 }
