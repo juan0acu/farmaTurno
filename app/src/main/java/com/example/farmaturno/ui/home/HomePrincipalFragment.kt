64ebeaa79ed968f2fc5f16e7293b484d0f88a7e7
Juan Acuna <juan0acu@gmail.com>
2023-09-26 00:05:37 -0300

    Codigo del SearchView, falta ver como se autocompleta

--- app/src/main/java/com/example/farmaturno/ui/home/HomePrincipalFragment.kt
+++ app/src/main/java/com/example/farmaturno/ui/home/HomePrincipalFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.farmaturno.databinding.FragmentHomePrincipalBinding
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse
 import com.example.farmaturno.ui.home.recyclerview.FarmaAdapter
+import com.google.android.gms.common.api.ApiException
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -34,6 +37,7 @@ class HomePrincipalFragment : Fragment() {
         init_observables()
         setupRecyclerView()
         listar_recycler()
+        init_listener()
     }
 
     private fun initViews() {
@@ -57,6 +61,35 @@ class HomePrincipalFragment : Fragment() {
             adapter.listadoFarmacias = farmacias
             adapter.notifyDataSetChanged()
         }
+    }
+
+    private fun init_listener(){
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (!newText.isNullOrEmpty()) {
+                    // Realizar una solicitud de autocompletado usando el ViewModel
+                    viewModel.getAutocompletePredictions(newText)
+                        .addOnCompleteListener { autocompletePredictions ->
+                            if (autocompletePredictions.isSuccessful) {
+                                val predictions: FindAutocompletePredictionsResponse? =
+                                    autocompletePredictions.result
+                                if (predictions != null) {
+                                    // Aquí puedes mostrar las sugerencias en un ListView o RecyclerView
+                                }
+                            } else {
+                                val exception: ApiException? =
+                                    autocompletePredictions.exception as ApiException?
+                                exception?.printStackTrace()
+                            }
+                        }
+                }
+                return true
+            }
+        })
     }
 
 }
